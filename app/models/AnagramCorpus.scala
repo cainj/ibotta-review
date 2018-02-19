51e818c00d5034dd4ae6ccd8eb8aeee3581d0532
James Cain <cain.jay@gmail.com>
2018-02-18 22:36:12 -0700

    sorting functions.

--- app/models/AnagramCorpus.scala
+++ app/models/AnagramCorpus.scala
@@ -48,46 +48,6 @@ class AnagramCorpus @Inject() (dictionary: Dictionary) extends Actor {
     f
   }
 
-  /**
-   * Stores the words in the corpus
-   *
-   * @param words The list of anagrams
-   * @return
-   */
-  def store(words: Anagrams): Future[Boolean] = {
-    val promise: Promise[Boolean] = Promise.apply()
-    val f = promise.future
-
-    val lowerCase = words.map(_.toLowerCase)
-    //validate that they are english words
-
-    if (lowerCase.forall(dictionary.valid)) {
-      //validate that they are anagrams
-      val key = lowerCase.head.sorted
-      if (isAnagrams(lowerCase, key)) {
-        store = store + (key -> words)
-        analyzer ! Analyze(store)
-        promise success true
-      } else sender() ! akka.actor.Status.Failure(new IllegalStateException("Doesn't satisfy anagrams.  https://en.wikipedia.org/wiki/Anagram"))
-    } else sender() ! akka.actor.Status.Failure(new IllegalArgumentException("Invalid word found in the list."))
-    f
-  }
-
-  /**
-   * Gets the anagrams
-   *
-   * @param word The word.
-   * @param limit The number of anagrams to get.
-   * @return
-   */
-  def get(word: String, limit: Option[Int] = None): Future[Anagrams] = {
-    val promise: Promise[Anagrams] = Promise.apply()
-    val f = promise.future
-    val anagrams = findAnagrams(word)
-    promise success (anagrams take limit.getOrElse(anagrams.size))
-    f
-  }
-
   def deleteWord(word: String): Future[Boolean] = {
     val promise: Promise[Boolean] = Promise.apply()
     val f = promise.future
@@ -109,6 +69,21 @@ class AnagramCorpus @Inject() (dictionary: Dictionary) extends Actor {
     f
   }
 
+  /**
+    * Gets the anagrams
+    *
+    * @param word The word.
+    * @param limit The number of anagrams to get.
+    * @return
+    */
+  def get(word: String, limit: Option[Int] = None): Future[Anagrams] = {
+    val promise: Promise[Anagrams] = Promise.apply()
+    val f = promise.future
+    val anagrams = findAnagrams(word)
+    promise success (anagrams take limit.getOrElse(anagrams.size))
+    f
+  }
+
   def reset: Future[Boolean] = {
     val promise: Promise[Boolean] = Promise.apply()
     val f = promise.future
@@ -118,6 +93,31 @@ class AnagramCorpus @Inject() (dictionary: Dictionary) extends Actor {
     f
   }
 
+  /**
+    * Stores the words in the corpus
+    *
+    * @param words The list of anagrams
+    * @return
+    */
+  def store(words: Anagrams): Future[Boolean] = {
+    val promise: Promise[Boolean] = Promise.apply()
+    val f = promise.future
+
+    val lowerCase = words.map(_.toLowerCase)
+    //validate that they are english words
+
+    if (lowerCase.forall(dictionary.valid)) {
+      //validate that they are anagrams
+      val key = lowerCase.head.sorted
+      if (isAnagrams(lowerCase, key)) {
+        store = store + (key -> words)
+        analyzer ! Analyze(store)
+        promise success true
+      } else sender() ! akka.actor.Status.Failure(new IllegalStateException("Doesn't satisfy anagrams.  https://en.wikipedia.org/wiki/Anagram"))
+    } else sender() ! akka.actor.Status.Failure(new IllegalArgumentException("Invalid word found in the list."))
+    f
+  }
+  
   private[this] def key(word: String) = word.toLowerCase.sorted
 
   private def isAnagrams(test: Set[String], key: String) = test forall (_.sorted == key)
